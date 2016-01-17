0b8f5c981a79825948c6e5630dfaf3b63928dc9e
kahopoon <kahopoon@gmail.com>
2016-01-18 00:35:55 +0800

    Update p_school_info_GUI.py

--- p_school_info_GUI.py
+++ p_school_info_GUI.py
@@ -2,30 +2,61 @@
 import urllib2
 import re
 import codecs
-from Tkinter import *
 import csv
+import time
+from Tkinter import *
 import tkFileDialog
 
 #content from
 request_url = "http://applications.chsc.hk/psp2015/sch_detail1.php?lang_id=2&sch_id="
-#sch_id sequence
+#sch_id initial sequence
 sequence = 1
-#retry maximum
-retry_max = 100
-#retry count
-retry = 0
-#checking status
-checking = False
-#re-run time for checking
-rerun = 2
+#maximum no data tolerate on continous sequence
+go_on_max = 10
+#count on no data continous sequence
+go_on = 0
+#exception item max try
+item_exception_max = 5
+#exception try hold time in seconds
+item_exception_holdtime = 0.3
+#last known good collected data id
+last_good = 0
 #all data
 all_schoolinfo = []
+#item exception remain list
+item_exception_list = []
+   
+#put raw data row into 'raw_data_row' in list
+def getRawData(input):
+    global go_on, last_good, item_exception_list
+    item_try_remain = item_exception_max
+    while item_try_remain >= 0:
+        try:
+            html_open = urllib2.urlopen(request_url + str(input))
+        except Exception:
+            item_try_remain -= 1
+            if item_try_remain < 0:
+                item_exception_list.append(input)
+            time.sleep(item_exception_holdtime)  
+            continue
+        else:      
+            html_read = html_open.read()
+            html_data = unicode(html_read,'UTF-8').encode('UTF-8')  
+            find_school_name = re.search('<td colspan="4" align="left"><span class="sch_detail_header">' + '(.+?)' + '</span>', html_data) 
+            if find_school_name:
+                school_name = find_school_name.group(1)
+                label_latest_content.set("找到 " + school_name.strip() + " ！")
+                root.update()
+                last_good = input
+                go_on = 0
+                return html_data
+            else:
+                go_on += 1
+                return None
 
 #write every row of all_schoolinfo into csv
 def outputFile():
     headers = ['中文名稱','英文名稱','校網編號','地址','電話','電郵', '傳真', '網址', '校監／學校管理委員會主席', '是否已成立法團校董會', '校長', '學校類別', '學生性別', '辦學團體', '宗教', '創校年份', '校訓', '學校佔地面積', '一條龍中學', '直屬中學', '聯繫中學', '教學語言', '校車服務', '家長教師會', '舊生會/校友會', '法團校董會名稱']
-    label_allscanned_content.set("結束檢查，輸出" + str(len(all_schoolinfo)) + "項紀錄至檔案 ......")
-    root.update()
     file_type = [('Comma Separated values', '*.csv'), ]
     file_name = tkFileDialog.asksaveasfilename(filetypes=file_type, title="另存...")
     with open(file_name,'wb') as f:
@@ -33,53 +64,23 @@ def outputFile():
         f.write(codecs.BOM_UTF8)
         f_csv.writerow(headers)
         f_csv.writerows(all_schoolinfo)        
-    label_output_content.set("輸出完成。再見！")
-
-#determine to recall getInformation for checking
-def checkData():
-    global rerun, sequence, retry
-    rerun -= 1
-    sequence = 1
-    retry = 0
-    label_allscanned_content.set("收集完畢，重新檢查 ......(剩餘" + str(rerun) + "次)")
+    label_output_content.set("已輸出 " + str(len(all_schoolinfo)) + " 項紀錄至CSV檔案")
     root.update()
-    root.after(0, getInformation(sequence))
-
-#put raw data row into 'raw_data_row' in list
-def getRawData(input):
-    global retry   
-    try:
-        html_open = urllib2.urlopen(request_url + str(input))
-    except Exception:
-        label_latest_content.set("ID: " + str(input) + " 沒有資料, 嘗試下一個......(" + str(retry_max - retry) + "次後結束)")
-        retry += 1
-        return None
-    else:         
-        html_read = html_open.read()
-        html_data = unicode(html_read,'UTF-8').encode('UTF-8')
-        
-        find_school_name = re.search('<td colspan="4" align="left"><span class="sch_detail_header">' + '(.+?)' + '</span>', html_data) 
-        if find_school_name:       
-            school_name = find_school_name.group(1)
-            label_latest_content.set("找到 " + school_name + " ！")
-            retry = 0
-            return html_data
-        else:
-            retry += 1
-            return None
-            
+           
 #get school info, encapsulate into tuple row, and append the row to all_schoolinfo
 def getInformation(input):
-    global all_schoolinfo, sequence, checking
+    global all_schoolinfo, sequence
     raw_data = getRawData(str(input))
     if raw_data != None:
         schoolinfo_row = []    
         find_school_name = re.findall('<span class="sch_detail_header">' + '(.+?)' + '</span>', raw_data)
-        school_chi_name = find_school_name[0]
-        school_eng_name = find_school_name[1]    
+        school_chi_name = find_school_name[0].strip()
+        school_eng_name = find_school_name[1].strip()
         find_school_poa = re.findall('<span class="sch_detail_poa">' + '(.+?)' + '</span>', raw_data)
-        school_poa = find_school_poa[1]    
+        school_poa = find_school_poa[1].strip()
         find_school_info = re.findall('<span class="sch_detail_info">' + '(.+?)' + '</span>', raw_data)
+        for loop in range(len(find_school_info)):
+            find_school_info[loop] = find_school_info[loop].strip()
         school_addr = find_school_info[1]
         school_tel = find_school_info[3]
         school_email = find_school_info[5]
@@ -112,27 +113,26 @@ def getInformation(input):
         schoolinfo_row.extend((school_chi_name, school_eng_name, school_poa, school_addr, school_tel, school_email, school_fax, school_url,
         school_chancellor, school_imc, school_principal, school_mode, school_studentGender, school_sponsor, school_religious,
         school_opened, school_slogan, school_areaSize, school_directthrough, school_direct_S_school, school_connect_S_school,
-        school_tLanguage, school_bus, school_pta, school_alumni, school_imcName))   
-        if not checking:
-            all_schoolinfo.append(tuple(schoolinfo_row))
-        else:
-            for loop in all_schoolinfo:
-                if schoolinfo_row[0] == loop[0]:
-                    break
-                if loop is all_schoolinfo[-1]:
-                    all_schoolinfo.append(tuple(schoolinfo_row))
+        school_tLanguage, school_bus, school_pta, school_alumni, school_imcName))  
+        all_schoolinfo.append(tuple(schoolinfo_row)) 
     sequence += 1
-    root.update()
-    if retry < retry_max:
-        root.after(0, getInformation(sequence))
+
+#tells data correctness before output
+def dataReport():
+    error_found = [s for s in item_exception_list if s < last_good]
+    if not error_found:
+        label_report_content.set("沒有資料錯誤，準備輸出...")
     else:
-        if rerun > 0:
-            checking = True
-            root.after(0, checkData)
-        else:
-            checking = False
-            root.after(0, outputFile)
-    
+        label_report_content.set(str(len(error_found)) + " 項錯誤資料未能修正，準備輸出")
+    root.update()
+          
+def start():
+    label_latest_content.set("開始收集資料，請稍候...")
+    while go_on < go_on_max:
+        getInformation(sequence)
+    dataReport()
+    outputFile()
+
 #call GUI
 root = Tk()
 #windows box non-resizable and fixes with supplied width height attributes
@@ -145,11 +145,11 @@ frame.pack()
 #label show on gui
 label_latest_content = StringVar()
 Label(frame, textvariable=label_latest_content, justify=LEFT).pack()
-label_allscanned_content = StringVar()
-Label(frame, textvariable=label_allscanned_content, justify=LEFT).pack()
+label_report_content = StringVar()
+Label(frame, textvariable=label_report_content, justify=LEFT).pack()
 label_output_content = StringVar()
 Label(frame, textvariable=label_output_content, justify=LEFT).pack()
 #initial function call
-root.after(0, getInformation(sequence))
+root.after(0, start)
 #GUI start
 root.mainloop()
